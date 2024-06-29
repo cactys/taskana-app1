a5c413ad7967f53622cde42a6218af5bba6d6b91
Egor Osipchuk <osipchuk.egor-osipchuck@yandex.ru>
2024-06-30 00:20:53 +0700

    todo-list

---
+++ main.js
@@ -0,0 +1,6 @@
+inputSearch = document.querySelector('#header-search')
+buttonSearch = document.querySelector('.header__form-button')
+
+buttonSearch.addEventListener('click',function(){
+    console.log(inputSearch.value)
+});
