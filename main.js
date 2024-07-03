ad4bf9f8b3a88fd8bb4e83f45b9e15e4d7bdb9b8
Egor Osipchuk <osipchuk.egor-osipchuck@yandex.ru>
2024-07-03 23:06:27 +0700

    commit_weather

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 inputSearch = document.querySelector('#header-search')
-buttonSearch = document.querySelector('.header__form-button')
+buttonSearch = document.querySelector('.button-search')
+buttonClose = document.querySelector('.button-dagger')
 
-buttonSearch.addEventListener('click',function(){
-    console.log(inputSearch.value)
-});
+
+inputSearch.addEventListener('input', () => {
+    
+
+    if (inputSearch.value !== '') {
+        buttonClose.classList.remove('none')
+        buttonSearch.classList.add('none')
+    } else {
+        buttonClose.classList.add('none')
+        buttonSearch.classList.remove('none')
+    }
+
+    console.log(inputSearch.value);
+})
