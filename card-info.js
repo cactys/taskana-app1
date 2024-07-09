8c5b50167b8763c4e7c6b7be8cabe6444e2c7af0
Egor Osipchuk <osipchuk.egor-osipchuck@yandex.ru>
2024-07-09 17:55:01 +0700

    debugging-2-done-1

--- card-info.js
+++ card-info.js
@@ -1,43 +1,44 @@
-const humidity = document.querySelector('#humidity');
-const barometr = document.querySelector('#barometr');
-const visibility = document.querySelector('#visibility');
-const sunrise = document.querySelector('#sunrise');
-const sunset = document.querySelector('#sunset');
-const wind = document.querySelector('#wind');
-const barometrComment = document.querySelector('#barometr-comment');
-const visibilityComment = document.querySelector('#visibility-comment');
-const sunriseComment = document.querySelector('#sunrise-comment');
-const sunsetComment = document.querySelector('#sunset-comment');
-const windComment = document.querySelector('#wind-comment');
-const inputRange = document.querySelector('#range');
-
 
 const anyVariable =  {
-    humidity : '22',
+    humidity : '70',
     barometr : '761',
-    visibility :'28',
+    visibility :'30',
     sunrise: '8:42',
     sunset: '16:37',
-    wind: '2',
+    wind: '3',
     barometrComment:'Повышенное',
     visibilityComment:'Нормальная',
     sunriseComment:'02:47',
-    sunsetComment:'05:08',
+    sunsetComment:'06:08',
     windComment:'Cеверо-западный',
 }
+const anyValues =  {
+    humidity : document.querySelector('#humidity'),
+    barometr : document.querySelector('#barometr'),
+    visibility : document.querySelector('#visibility'),
+    sunrise: document.querySelector('#sunrise'),
+    sunset: document.querySelector('#sunset'),
+    wind: document.querySelector('#wind'),
+    barometrComment: document.querySelector('#barometr-comment'),
+    visibilityComment:document.querySelector('#visibility-comment'),
+    sunriseComment:  document.querySelector('#sunrise-comment'),
+    sunsetComment:document.querySelector('#sunset-comment'),
+    windComment:document.querySelector('#wind-comment'),
+}
+
 
 // заполняем значениям
-humidity.innerText = anyVariable.humidity
-barometr.innerText = anyVariable.barometr
-visibility.innerText = anyVariable.visibility
-sunrise.innerText = anyVariable.sunrise
-sunset.innerText = anyVariable.sunset
-wind.innerText = anyVariable.wind
-barometrComment.innerText = anyVariable.barometrComment
-visibilityComment.innerText = anyVariable.visibilityComment
-sunriseComment.innerText = anyVariable.sunriseComment
-sunsetComment.innerText = anyVariable.sunsetComment
-windComment.innerText = anyVariable.windComment
-inputRange.value = anyVariable.humidity
+anyValues.humidity.innerText = anyVariable.humidity
+anyValues.barometr.innerText = anyVariable.barometr
+anyValues.visibility.innerText = anyVariable.visibility
+anyValues.sunrise.innerText = anyVariable.sunrise
+anyValues.sunset.innerText = anyVariable.sunset
+anyValues.wind.innerText = anyVariable.wind
+anyValues.barometrComment.innerText = anyVariable.barometrComment
+anyValues.visibilityComment.innerText = anyVariable.visibilityComment
+anyValues.sunriseComment.innerText = anyVariable.sunriseComment
+anyValues.sunsetComment.innerText = anyVariable.sunsetComment
+anyValues.windComment.innerText = anyVariable.windComment
+
 
 
--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const inputSearch = document.querySelector('#header-search')
-const buttonSearch = document.querySelector('.button-search')
-const buttonClose = document.querySelector('.button-dagger')
+const buttonSearch = document.querySelector('.search')
+const buttonClose = document.querySelector('.dagger')
 const form = document.querySelector('#form')
 
 
 form.addEventListener('input', () => {
-    
-
-    if (inputSearch.value !== '') {
-        buttonClose.classList.remove('none')
-        buttonSearch.classList.add('none')
-    } else {
-        buttonClose.classList.add('none')
-        buttonSearch.classList.remove('none')
-    }
-
     console.log(inputSearch.value);
 })
+
+buttonClose.addEventListener('click',(event) =>{
+    event.preventDefault()
+    inputSearch.value = '';
+    buttonClose.classList.add('none')
+    buttonSearch.classList.remove('none')
+})
+
+inputSearch.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        console.log(inputSearch.value);
+    }
+});
+
+inputSearch.addEventListener('focus', () => {
+    buttonClose.classList.remove('none')
+    buttonSearch.classList.add('none')
+   
+})
+
