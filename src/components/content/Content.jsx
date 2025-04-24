86852f5d8f1c4e5cf2bbfaaeff0c22879f7e4328
cactys <cactys95@yandex.ru>
2025-04-24 17:25:28 +0300

    fix: footer, content

---
+++ src/components/content/Content.jsx
@@ -0,0 +1,21 @@
+import IncomingTasks from '@pages/incomingTasks/IncomingTasks';
+import NavBar from '@components/UI/navBar/NavBar';
+import Statistic from '../statistic/Statistic';
+
+import styles from './Content.module.css';
+
+/**
+ * Контейнер основного содержимого приложения
+ * @returns {JSX.Element} - JSX элемент страницы
+ */
+const Content = () => {
+  return (
+    <div className={styles.content}>
+      <NavBar />
+      <IncomingTasks />
+      <Statistic />
+    </div>
+  );
+};
+
+export default Content;
