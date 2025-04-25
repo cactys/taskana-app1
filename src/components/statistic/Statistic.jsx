2096ca6585abae21a2ebcb0e4bda3aa0525033ea
cactys <cactys95@yandex.ru>
2025-04-25 09:04:51 +0300

    task-done

---
+++ src/components/statistic/Statistic.jsx
@@ -0,0 +1,23 @@
+import Illustration from '@components/illustration/Illustration';
+
+import styles from './Statistic.module.css';
+
+/**
+ * Компонент статистики
+ * @returns {JSX.Element} - JSX элемент статистики
+ */
+const Statistic = () => {
+  return (
+    <section className={`${styles.statistic}`}>
+      <div className={styles.statisticContainer}>
+        <Illustration id="notebookImage" className={styles.statisticImage} />
+        <p className={styles.statisticText}>
+          Здесь мы поможем тебе управлять твоими задачами, отслеживать
+          статистику и&nbsp;самочувствие.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Statistic;
